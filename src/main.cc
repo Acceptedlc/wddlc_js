ab901acd916d75a1f8497d04e1015ff6b55e4d37
Acceptedlc <Accepted@icloud.com>
2017-06-07 13:58:51 +0800

    test string

--- src/main.cc
+++ src/main.cc
@@ -1,20 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "include/libplatform/libplatform.h"
-#include "include/v8.h"
+#include <string>
+#include <iostream>
 
-using namespace v8;
-
-
+using namespace std;
 
 int main(int argc, char *argv[]) {
+    string bootstrap_js_code = "(function(process){
+        process.log('hello world');
+    })";
+    count << boobootstrap_js_code << endl;
+    return  1；
+}
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include "include/libplatform/libplatform.h"
+// #include "include/v8.h"
+
+
+// using namespace v8;
+
+
+
+
+
+// int main(int argc, char *argv[]) {
+    /**
+    现在是namespace被卡住了，不知道怎么同时用v8和std这两个namespace
+    **/
+
     //1.read file -> 复习一下C的文件调用API
     //2.config process object
     //3.compile and run file
     //4.run return function
 
 
-    return 0;
-}
+
+
+
+
+
+//     return 0;
+// }
 
